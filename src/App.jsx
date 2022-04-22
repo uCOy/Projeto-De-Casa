c21a2514c6e45267574008d42f6d2bb969614c2f
Richard <richardcoutinho04@gmail.com>
2022-04-22 15:15:40 -0300

    correção 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
+import Index from './Body/Body';
 
 function App() {
   return (
     <div>
       <Header/>
+      <Index/>
       <Footer/>
     </div>
   );
