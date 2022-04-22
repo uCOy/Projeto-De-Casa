0fb395524f828bc680a1f56afe12041ddcd07284
Richard <richardcoutinho04@gmail.com>
2022-04-22 00:53:16 -0300

    Versão 22.04.2022

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
+
